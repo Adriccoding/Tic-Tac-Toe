13fe19ffc2636e3646b22dd23fce8ed4e1f2243b
Chris Adrion <Adric@farmingdale.edu>
2020-07-29 15:09:37 -0400

    Added restart game and turn tracker

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +14,15 @@ public class MainActivity extends AppCompatActivity {
             {R.id.button10, R.id.button11, R.id.button12},
             {R.id.button20, R.id.button21, R.id.button22}};
     Button tiles[][] = new Button[3][3];
+    private TextView tracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonRestart = (Button) findViewById(R.id.buttonRestart);
+        tracker = (TextView) findViewById(R.id.Tracker);
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -25,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
                 tiles[i][j].setOnClickListener(write);
             }
         }
+        buttonRestart.setOnClickListener(restart);
     }
 
     View.OnClickListener write = new View.OnClickListener() {
@@ -35,11 +41,27 @@ public class MainActivity extends AppCompatActivity {
                 b.setText("X");
                 isX = false;
                 b.setEnabled(false);
+                tracker.setText("O");
             } else {
                 b.setText("O");
                 isX = true;
                 b.setEnabled(false);
+                tracker.setText("X");
             }
+        }
+    };
+
+    View.OnClickListener restart = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    tiles[i][j].setText("");
+                    tiles[i][j].setEnabled(true);
+                }
+            }
+            isX = true;
+            tracker.setText("X");
         }
     };
 }
