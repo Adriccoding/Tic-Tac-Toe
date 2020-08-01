373272f9f3206df1dfe50a93cc5a7fa01cd1a560
Chris Adrion <Adric@farmingdale.edu>
2020-07-31 23:24:58 -0400

    added win counter and landscape (landscape has an app crashing bug)

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.tic_tac_toe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,13 +11,25 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //saved state key-values
+    private static final String STATE_TURN_TRACKER = "turnTracker";
+    private static final String STATE_DRAW = "isX";
+    private static final String STATE_X_WINS = "playerXWins";
+    private static final String STATE_O_WINS = "playerOWins";
+
+
+    // Global variables
+    int playerXWins = 0;
+    int playerOWins = 0;
     boolean isX = true;
+    String turnTracker = "X";
     private static final int[][] idArray = {{R.id.button00, R.id.button01, R.id.button02},
             {R.id.button10, R.id.button11, R.id.button12},
             {R.id.button20, R.id.button21, R.id.button22}};
     Button tiles[][] = new Button[3][3];
     private TextView tracker;
-    private TextView test;
+    private TextView XTracker;
+    private TextView OTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
         Button buttonRestart = (Button) findViewById(R.id.buttonRestart);
         tracker = (TextView) findViewById(R.id.Tracker);
-        test = (TextView) findViewById(R.id.test);
+        XTracker = (TextView) findViewById(R.id.XwinCounter);
+        OTracker = (TextView) findViewById(R.id.OwinCounter);
+        tracker.setText(turnTracker);
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -40,15 +56,17 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             Button b = (Button) v;
             if (isX == true) {
-                b.setText("X");
+                b.setText(turnTracker);
                 isX = false;
                 b.setEnabled(false);
-                tracker.setText("O");
+                turnTracker = "O";
+                tracker.setText(turnTracker);
             } else {
-                b.setText("O");
+                b.setText(turnTracker);
                 isX = true;
                 b.setEnabled(false);
-                tracker.setText("X");
+                turnTracker = "X";
+                tracker.setText(turnTracker);
             }
             checkForWin();
         }
@@ -65,34 +83,86 @@ public class MainActivity extends AppCompatActivity {
             }
             isX = true;
             tracker.setText("X");
-            test.setText("No Winner");
+            turnTracker = "X";
         }
     }; // can make this its own function if need to reset game in other ways
 
-    private void checkForWin(){
-        for(int i=0; i<3; i++){
-            if(tiles[i][0].getText().toString().equals(tiles[i][1].getText().toString())
+    @SuppressLint("DefaultLocale")
+    private void checkForWin() {
+        for (int i = 0; i < 3; i++) {
+            if (tiles[i][0].getText().toString().equals(tiles[i][1].getText().toString())
                     && (tiles[i][0].getText().toString().equals(tiles[i][2].getText().toString()))
-                    && !tiles[i][0].getText().toString().equals("")){
-                test.setText("Winner!");
+                    && !tiles[i][0].getText().toString().equals("")) {
+                if (tiles[i][0].getText().toString().equals("X")) {
+                    playerXWins++;
+                    XTracker.setText(String.format("%d", playerXWins));
+                }
+                if (tiles[i][0].getText().toString().equals("O")) {
+                    playerOWins++;
+                    OTracker.setText(String.format("%d", playerOWins));
+                }
             }
         } // check horizontal victory
-        for(int i=0; i<3; i++){
-            if(tiles[0][i].getText().toString().equals(tiles[1][i].getText().toString())
+        for (int i = 0; i < 3; i++) {
+            if (tiles[0][i].getText().toString().equals(tiles[1][i].getText().toString())
                     && (tiles[0][i].getText().toString().equals(tiles[2][i].getText().toString()))
-                    && !tiles[0][i].getText().toString().equals("")){
-                test.setText("Winner!");
+                    && !tiles[0][i].getText().toString().equals("")) {
+                if (tiles[0][i].getText().toString().equals("X")) {
+                    playerXWins++;
+                    XTracker.setText(String.format("%d", playerXWins));
+                }
+                if (tiles[0][i].getText().toString().equals("O")) {
+                    playerOWins++;
+                    OTracker.setText(String.format("%d", playerOWins));
+                }
+
             }
         } // check vertical victory
-        if(tiles[0][0].getText().toString().equals(tiles[1][1].getText().toString())            /////////////////////////////////////
+        if (tiles[0][0].getText().toString().equals(tiles[1][1].getText().toString())            /////////////////////////////////////
                 && (tiles[0][0].getText().toString().equals(tiles[2][2].getText().toString()))  //    Check top left to bottom right win
-                && !tiles[0][0].getText().toString().equals("")){                               //
-            test.setText("Winner!");                                                            /////////////////////////////////////
+                && !tiles[0][0].getText().toString().equals("")) {                               //
+            if (tiles[0][0].getText().toString().equals("X")) {                                   /////////////////////////////////////
+                playerXWins++;
+                XTracker.setText(String.format("%d", playerXWins));
+            }
+            if (tiles[0][0].getText().toString().equals("O")) {
+                playerOWins++;
+                OTracker.setText(String.format("%d", playerOWins));
+            }
         }
-        if(tiles[0][2].getText().toString().equals(tiles[1][1].getText().toString())            /////////////////////////////////////
+        if (tiles[0][2].getText().toString().equals(tiles[1][1].getText().toString())            /////////////////////////////////////
                 && (tiles[0][2].getText().toString().equals(tiles[2][0].getText().toString()))  //    Check top right to bottom left win
-                && !tiles[0][2].getText().toString().equals("")){                               //
-            test.setText("Winner!");                                                            /////////////////////////////////////
+                && !tiles[0][2].getText().toString().equals("")) {                               //
+            if (tiles[0][2].getText().toString().equals("X")) {                                   //////////////////////////////////////
+                playerXWins++;
+                XTracker.setText(String.format("%d", playerXWins));
+            }
+            if (tiles[0][2].getText().toString().equals("O")) {
+                playerOWins++;
+                OTracker.setText(String.format("%d", playerOWins));
+            }
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString(STATE_TURN_TRACKER, turnTracker);
+        outState.putBoolean(STATE_DRAW, isX);
+        outState.putInt(STATE_X_WINS, playerXWins);
+        outState.putInt(STATE_O_WINS, playerOWins);
+        super.onSaveInstanceState(outState);
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        turnTracker = savedInstanceState.getString(STATE_TURN_TRACKER);
+        isX = savedInstanceState.getBoolean(STATE_DRAW);
+        playerXWins = savedInstanceState.getInt(STATE_X_WINS);
+        playerOWins = savedInstanceState.getInt(STATE_O_WINS);
+        tracker.setText(turnTracker);
+        //OTracker.setText(String.format("%d", playerOWins));
+        //XTracker.setText(String.format("%d", playerXWins));
     }
 }
